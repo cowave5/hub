407aaa86bbbe84b577e0ffb60d2d8414236fc3e9
shanhm1991 <shanhm1991@163.com>
2024-09-24 10:12:30 +0800

    init sys-eureka

---
+++ sys-eureka/src/main/java/com/cowave/sys/eureka/Application.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2017～2099 Cowave All Rights Reserved.
+ *
+ * For licensing information, please contact: https://www.cowave.com.
+ *
+ * This code is proprietary and confidential.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ */
+package com.cowave.sys.eureka;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ * @author shanhm1991
+ *
+ */
+@EnableAdminServer
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
