cowave5/sys

引入xxl-job -> sys-job

sys-job/sys-job-app/src/main/java/com/cowave/sys/job/app/JobClientController.java
@@ -1,0 +1,49 @@
+package com.cowave.sys.job.app;
+
+import com.cowave.commons.client.http.response.Response;
+import com.cowave.sys.job.domain.request.ClientRegistry;
+import com.cowave.sys.job.domain.request.TriggerResponse;
+import com.cowave.sys.job.service.JobClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author xuxueli/shanhuiming
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/client")
+public class JobClientController {
+
+    private final JobClientService jobClientService;
+
+    /**
+     * 注册
+     */
+    @PostMapping("/registry")
+    public Response<Void> registry(@Validated @RequestBody ClientRegistry clientRegistry) throws Exception {
+        return Response.success(() -> jobClientService.registry(clientRegistry));
+    }
+
+    /**
+     * 注销
+     */
+    @PostMapping("/unregistry")
+    public Response<Void> unRegistry(@Validated @RequestBody ClientRegistry clientRegistry) throws Exception {
+        return Response.success(() -> jobClientService.unRegistry(clientRegistry));
+    }
+
+    /**
+     * 回调
+     */
+    @PostMapping("/callback")
+    public Response<Void> callback(@Validated @RequestBody List<TriggerResponse> list) throws Exception {
+        return Response.success(() -> jobClientService.callback(list));
+    }
+}

sys-job/sys-job-client/src/main/java/com/cowave/sys/job/client/ServerService.java
@@ -1,0 +1,26 @@
+package com.cowave.sys.job.client;
+
+import com.cowave.commons.client.http.annotation.HttpClient;
+import com.cowave.commons.client.http.annotation.HttpHost;
+import com.cowave.commons.client.http.annotation.HttpLine;
+import com.cowave.commons.client.http.response.Response;
+import com.cowave.sys.job.domain.request.TriggerResponse;
+import com.cowave.sys.job.domain.request.ClientRegistry;
+
+import java.util.List;
+
+/**
+ * @author xuxueli/shanhuiming
+ */
+@HttpClient
+public interface ServerService {
+
+    @HttpLine("POST /job/client/registry")
+    Response<Void> registry(@HttpHost String httpUrl, ClientRegistry clientRegistry);
+
+    @HttpLine("POST /job/client/unregistry")
+    Response<Void> unRegistry(@HttpHost String httpUrl, ClientRegistry clientRegistry);
+
+    @HttpLine("POST /job/client/callback")
+    void callback(@HttpHost String httpUrl, List<TriggerResponse> list);
+}

sys-job/sys-job-domain/src/main/java/com/cowave/sys/job/domain/JobClient.java
@@ -1,0 +1,29 @@
+package com.cowave.sys.job.domain;
+
+import com.cowave.sys.job.domain.request.ClientRegistry;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author xuxueli/shanhuiming
+ */
+@NoArgsConstructor
+@Data
+public class JobClient {
+
+    private Integer id;
+
+    private String clientName;
+
+    private String clientAddress;
+
+    private Date updateTime;
+
+    public JobClient(ClientRegistry clientRegistry){
+        this.clientName = clientRegistry.getClientName();
+        this.clientAddress = clientRegistry.getClientAddress();
+        this.updateTime = new Date();
+    }
+}

sys-job/sys-job-domain/src/main/java/com/cowave/sys/job/domain/request/IdleCheck.java
@@ -1,0 +1,16 @@
+package com.cowave.sys.job.domain.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author xuxueli/shanhuiming
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class IdleCheck {
+
+    private int jobId;
+}

cowave5/hub

sys-job/sys-job-app/src/main/java/com/cowave/sys/job/app/controller/JobClientController.java
@@ -1,0 +1,51 @@
+package com.cowave.sys.job.app.controller;
+
+import com.cowave.commons.client.http.response.Response;
+import com.cowave.sys.job.domain.client.ClientRegistry;
+import com.cowave.sys.job.domain.client.TriggerResponse;
+import com.cowave.sys.job.service.JobClientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 客户端
+ *
+ * @author xuxueli/shanhuiming
+ */
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/client")
+public class JobClientController {
+
+    private final JobClientService jobClientService;
+
+    /**
+     * 注册
+     */
+    @PostMapping("/registry")
+    public Response<Void> registry(@Validated @RequestBody ClientRegistry clientRegistry) throws Exception {
+        return Response.success(() -> jobClientService.registry(clientRegistry));
+    }
+
+    /**
+     * 注销
+     */
+    @PostMapping("/unregistry")
+    public Response<Void> unRegistry(@Validated @RequestBody ClientRegistry clientRegistry) throws Exception {
+        return Response.success(() -> jobClientService.unRegistry(clientRegistry));
+    }
+
+    /**
+     * 回调
+     */
+    @PostMapping("/callback")
+    public Response<Void> callback(@Validated @RequestBody List<TriggerResponse> list) throws Exception {
+        return Response.success(() -> jobClientService.callback(list));
+    }
+}

sys-job/sys-job-client/src/main/java/com/cowave/sys/job/client/ServerService.java
@@ -1,0 +1,26 @@
+package com.cowave.sys.job.client;
+
+import com.cowave.commons.client.http.annotation.HttpClient;
+import com.cowave.commons.client.http.annotation.HttpHost;
+import com.cowave.commons.client.http.annotation.HttpLine;
+import com.cowave.commons.client.http.response.Response;
+import com.cowave.sys.job.domain.client.TriggerResponse;
+import com.cowave.sys.job.domain.client.ClientRegistry;
+
+import java.util.List;
+
+/**
+ * @author xuxueli/shanhuiming
+ */
+@HttpClient
+public interface ServerService {
+
+    @HttpLine("POST /job/client/registry")
+    Response<Void> registry(@HttpHost String httpUrl, ClientRegistry clientRegistry);
+
+    @HttpLine("POST /job/client/unregistry")
+    Response<Void> unRegistry(@HttpHost String httpUrl, ClientRegistry clientRegistry);
+
+    @HttpLine("POST /job/client/callback")
+    void callback(@HttpHost String httpUrl, List<TriggerResponse> list);
+}

sys-job/sys-job-domain/src/main/java/com/cowave/sys/job/domain/JobClient.java
@@ -1,0 +1,31 @@
+package com.cowave.sys.job.domain;
+
+import com.cowave.sys.job.domain.client.ClientRegistry;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * @author xuxueli/shanhuiming
+ */
+@NoArgsConstructor
+@Data
+public class JobClient {
+
+    private Integer id;
+
+    private String clientName;
+
+    private String clientAddress;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
+
+    public JobClient(ClientRegistry clientRegistry){
+        this.clientName = clientRegistry.getClientName();
+        this.clientAddress = clientRegistry.getClientAddress();
+        this.updateTime = new Date();
+    }
+}

sys-job/sys-job-domain/src/main/java/com/cowave/sys/job/domain/client/IdleCheck.java
@@ -1,0 +1,16 @@
+package com.cowave.sys.job.domain.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author xuxueli/shanhuiming
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class IdleCheck {
+
+    private int jobId;
+}